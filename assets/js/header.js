cfed690013b2f6018dc31370f4b98c0bee8808c7
leonardo-stefanini <stefanini.leonardo01@gmail.com>
2020-04-18 16:15:55 +0000

    aggiunto slider

--- assets/js/header.js
+++ assets/js/header.js
@@ -3,3 +3,37 @@
            $(".header__menu").toggleClass('is-open');
            e.preventDefault();
     });
+
+    /* Slider */
+
+    $(() => {
+       var mySwiper = new Swiper ('.swiper-container', {
+              navigation: {
+                     nextEl: '.swiper-button-next',
+                     prevEl: '.swiper-button-prev',
+              },
+               autoplay: {
+                     delay: 7000,
+                     disableOnInteraction: false,
+              },
+              direction: 'horizontal',
+              loop: true,
+              speed: 600,
+              effect: 'slide',
+       })
+     });
+
+     /* Header Transition */
+
+     $(() => {
+       $(document).scroll(() => {
+              let $nav = $(".header");
+              let $slider = $('.swiper-container')
+              $nav.toggleClass('dark', $(this).scrollTop() > $slider.height());
+              });
+
+       $(".header__icon-bar").click(() => {
+              $(".header").toggleClass('clicked');
+            });
+     });
+
