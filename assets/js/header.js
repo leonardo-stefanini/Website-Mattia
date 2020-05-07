8550c9cc047464d3f68e48f4e35f4cc524690861
leonardo-stefanini <stefanini.leonardo01@gmail.com>
2020-05-07 18:49:32 +0000

    Small Ui fixes

--- assets/js/header.js
+++ assets/js/header.js
@@ -1,8 +1,43 @@
 
-    $(".header__icon-bar").click((e) => {
-           $(".header__menu").toggleClass('is-open');
-           e.preventDefault();
-    });
+       $(".header__icon-bar").click((e) => {
+              let $menu = $(".header__menu")
+
+              $menu.toggleClass('is-open')
+              $menu.css('transition', 'all 400ms ease-in-out')
+
+              $(".header__menu__item a").toggleClass('is-open')
+              $("body").toggleClass('no-scroll')
+              $(".croce").toggleClass('appear')
+              
+              e.preventDefault()
+       });
+
+       $(".croce").click((e) => {
+              let $menu = $(".header__menu")
+
+              $menu.toggleClass('is-open')
+              $menu.css('transition', 'all 400ms ease-in-out')
+
+              $(".header__menu__item a").toggleClass('is-open')
+              $("body").toggleClass('no-scroll')
+
+              e.preventDefault()
+       });
+
+     /* Header Transition */
+
+     $(() => {
+       $(document).scroll(() => {
+              let $nav = $(".header")
+              let $text = $('.header__menu__item a')
+              let $hamburger = $('.header__icon-bar')
+              
+              $nav.toggleClass('normal', $(this).scrollTop() > 50)
+              $text.toggleClass('normal', $(this).scrollTop() > 50)
+              $hamburger.toggleClass('normal', $(this).scrollTop() > 50)
+              });
+     });
+
 
     /* Slider */
 
@@ -17,17 +52,4 @@
               speed: 600,
               effect: 'slide',
        })
-     });
-
-     /* Header Transition */
-
-     $(() => {
-       $(document).scroll(() => {
-              let $nav = $(".header")
-              let $text = $('.header__menu__item a')
-              let $hamburger = $('.header__icon-bar')
-              $nav.toggleClass('normal', $(this).scrollTop() > 50)
-              $text.toggleClass('normal', $(this).scrollTop() > 50)
-              $hamburger.toggleClass('normal', $(this).scrollTop() > 50)
-              });
      });
